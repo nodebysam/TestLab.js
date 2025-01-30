98a2b43c447996a4e3dec75a2eadd91e5620dabd
nodebysam <sam@bulletinfusion.com>
2025-01-29 19:15:21 -0700

    minor changes

--- index.js
+++ index.js
@@ -10,87 +10,9 @@
  * For full license details, see the LICENSE file.
  */
 
-const TestRunner = require('./src/runner/testRunner');
-const { config, setConfig } = require('./src/config');
+const test = require('./src/test');
 const execute = require('./src/executor');
-const { beforeAllTests, beforeTest, afterAllTests, afterTest } = require('./src/runner/setupHooks');
 
-const runner = new TestRunner();
-
-/**
- * Execute a test.
- * 
- * @param {string} description - The test description (i.e., 'Two plus two should equal 4').
- * @param {Function} fn - The test function to execute. 
- */
-let test = (description, fn) => {
-    const testFile = module.parent?.filename || 'unknown';
-    runner.test(description, fn, testFile);
-};
-
-test.beforeAllTests = (fn) => { beforeAllTests(fn); }
-test.beforeTest = (fn) => { beforeTest(fn); }
-test.afterTest = (fn) => { afterTest(fn); }
-test.afterAllTests = (fn) => { afterAllTests(fn); }
-
-test.setTestDirectory = (testDirectory) => {
-    setConfig({ testDirectory });
-};
-
-test.setTimeout = (timeout) => {
-    setConfig({ timeout });
-};
-
-test.setEnableReport = (enableReport) => {
-    setConfig({ enableReport });
-};
-
-test.setReportPath = (reportPath) => {
-    setConfig({ reportPath });
-};
-
-test.setReportFile = (reportFile) => {
-    setConfig({ reportFile });
-};
-
-test.setDebug = (debug) => {
-    setConfig({ debug });
-};
-
-test.testDirectory = () => {
-    return config.testDirectory;
-};
-
-test.timeout = () => {
-    return config.timeout;
-};
-
-test.reporter = () => {
-    return config.reporter;
-};
-
-test.enableReport = () => {
-    return config.enableReport;
-};
-
-test.reportPath = () => {
-    return config.reportPath;
-};
-
-test.reportFile = () => {
-    return config.reportFile;
-};
-
-test.debug = () => {
-    return config.debug;
-};
-
-test.execute = async () => {
-    await test.runTests();
-};
-
-if (require.main === module) {
-    (async () => { await execute(runner); });
-}
+(async() => { await execute(test.runner); });
 
 module.exports = test;
